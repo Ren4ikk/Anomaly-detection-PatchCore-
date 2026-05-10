3ad183e8bc62350420808487ca3717fa90875e8f
Ren4ikk <bakk5513@gmail.com>
2026-05-10 16:18:33 +0500

    Удалил CLI

--- inference.ps1
+++
@@ -1,6 +0,0 @@
-$ModelPath  = "./models/capsule.pt"
-$ImagePath  = "./mvtec_Defect_detection_dataset/capsule/train/good/020.png"
-$OutputDir  = "./results"
-$Threshold  = "0.86"
-
-poetry run python scripts/infer.py --model $ModelPath --image $ImagePath --output_dir $OutputDir
